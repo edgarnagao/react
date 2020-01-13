16f544a54f784fdd8a87d89d399eeb3b9ccab4d5
Edgar Montes <emontes@nova.edu>
2020-01-13 16:38:24 -0600

    Formulario

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import Header from './comonents/Header';
 
 import CategoriasProvider from '../src/context/CategoriaContext'
+import Formulario from './comonents/Formulario';
 
 
 function App() {
   return (
     <CategoriasProvider>
       <Header titulo="Evetos API"></Header>
+
+      <div className="uk-container">
+          <Formulario/>
+      </div>
+
+
     </CategoriasProvider>
   )
 }
